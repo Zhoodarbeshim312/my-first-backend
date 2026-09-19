c3a779e96edd649a9eb6838ce705a7d4126bb6f9
Zhoodarbeshim <joodarbeshimaktanov@gmail.com>
2026-09-19 21:31:06 +0600

    Remove build artifacts and generated files from repo

--- dist/app.d.ts
+++
@@ -1,3 +0,0 @@
-import "dotenv/config";
-declare const buildApp: () => import("express-serve-static-core").Express;
-export default buildApp;
--- dist/routes/index.d.ts
+++
@@ -1,2 +0,0 @@
-declare const globalRouter: import("express-serve-static-core").Router;
-export default globalRouter;
